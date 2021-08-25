ee7c4e7c3e6991aa775173d264e331c095ab2e66
noreply <noreply@github.com>
2021-08-25 17:23:28 +0800

    暂时取消sw

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
+// import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import naive from './plugins/naive-ui.ts'
