8df31d970086a02dbe10553b476e2ac9c12878de
inori-lover <337938188@qq.com>
2021-08-27 00:51:09 +0800

    feat: 切换vuex为pinia，定义store命名规则；书写简单示例

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import './assets/style/main.scss'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
 import { naive } from './plugins/naive-ui'
 
 const app = createApp(App)
-app.use(store).use(router).use(naive).mount('#app')
+app.use(createPinia()).use(router).use(naive).mount('#app')
