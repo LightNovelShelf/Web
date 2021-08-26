edd14502d8a05ae0c1925609a1cc6dc9042ee774
wuyu <wuyu8512@gmail.com>
2021-08-26 21:35:07 +0800

    抽离bookCard组件

---
+++ src/components/bookCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <n-card content-style="padding: 4px;">
+    <template #cover>
+      <div
+        :style="{
+          paddingBottom: '150%',
+          backgroundImage: `url('${book.cover}')`,
+          backgroundSize: 'cover'
+        }"
+      >
+      </div>
+      <n-element tag="div" class="book-tag">
+        <span style="font-size: 12px">录入</span>
+      </n-element>
+    </template>
+    <div class="book-name">
+      <div class="book-name-text" :title="book.name">
+        {{ book.name }}
+      </div>
+    </div>
+  </n-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'bookCard',
+  props: {
+    book: Object
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/mixin';
+
+.book-tag {
+  position: absolute;
+  top: 6px;
+  right: 0;
+  background-color: var(--primary-color);
+  color: white;
+  padding: 0 6px;
+  border-radius: 1em 0 0 1em;
+}
+
+.book-name {
+  display: flex;
+  align-items: center;
+  --font-size: 12px;
+  font-size: var(--font-size);
+  height: calc(var(--font-size) * var(--line-height) * 2);
+
+  .book-name-text {
+    @include ellipsis(2);
+  }
+}
+</style>
--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,27 +1,36 @@
 <template>
-  <div style="max-width: 80vw;margin: 0 auto">
-    <n-space justify="space-around" size="large">
-      <div>
-        <n-cascader
-          v-model="value"
-          :options="options"
-          :cascade="cascade"
-          :leaf-only="leafOnly"
-          :show-path="showPath"
-          :filterable="filterable"
-          placeholder="排序方式"
-          style="min-width: 100px"
-        />
-      </div>
+  <div style="max-width: 80vw; margin: 0 auto">
+    <div style="display: flex; justify-content: space-around">
+      <n-cascader
+        v-model:value="value"
+        :options="options"
+        :cascade="cascade"
+        :leaf-only="leafOnly"
+        :show-path="showPath"
+        :filterable="filterable"
+        placeholder="排序方式"
+        style="max-width: 150px"
+      />
+
       <n-pagination v-model:page="page" :page-count="100" />
-    </n-space>
+    </div>
+
+    <n-grid :x-gap="12" :y-gap="8" cols="3 700:4 1000:5 1300:6" style="padding-top: 24px">
+      <n-grid-item v-for="(book, index) in bookData" :key="index">
+        <book-card :book="book"></book-card>
+      </n-grid-item>
+    </n-grid>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import bookCard from "@/components/bookCard";
 
 export default defineComponent({
+  components:{
+    bookCard
+  },
   name: 'Book',
   data() {
     return {
@@ -63,7 +72,33 @@ export default defineComponent({
           ]
         }
       ],
-      page: 1
+      page: 2,
+      bookData: [
+        {
+          cover: 'https://neptune.noire.cc:233/images/2021/08/22/780cd596dc21.webp',
+          name: '我是书名，我很短'
+        },
+        {
+          cover: 'https://neptune.noire.cc:233/images/2021/08/22/16d63ec3cae1.md.webp',
+          name: '我是书名，我很长很长很长很长很长很长长很长很长长很长很长长很长很长长很长很长'
+        },
+        {
+          cover: 'https://neptune.noire.cc:233/images/2021/08/22/780cd596dc21.webp',
+          name: '我是书名，我很长很长很长很长很长很长长很长很长长很长很长长很长很长长很长很长'
+        },
+        {
+          cover: 'https://neptune.noire.cc:233/images/2021/08/22/16d63ec3cae1.md.webp',
+          name: '我是书名，我很长很长很长很长很长很长长很长很长长很长很长长很长很长长很长很长'
+        },
+        {
+          cover: 'https://neptune.noire.cc:233/images/2021/08/22/780cd596dc21.webp',
+          name: '我是书名，我很长很长很长很长很长很长长很长很长长很长很长长很长很长长很长很长'
+        },
+        {
+          cover: 'https://neptune.noire.cc:233/images/2021/08/22/16d63ec3cae1.md.webp',
+          name: '我是书名，我很长很长很长很长很长很长长很长很长长很长很长长很长很长长很长很长'
+        }
+      ]
     }
   },
   setup() {
@@ -72,4 +107,6 @@ export default defineComponent({
 })
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+
+</style>
