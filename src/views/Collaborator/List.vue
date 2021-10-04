c5c14da66bfd2e52c3a21e9ab9e7e6a9d790037d
inori_lover <inori_lover@outlook.com>
2021-10-04 12:32:11 +0800

    feat: 增加mock数据类型；监听容器宽度变化重新排版

--- src/views/Collaborator/List.vue
+++ src/views/Collaborator/List.vue
@@ -11,10 +11,12 @@ import CardItem from './components/Card.vue'
 
 /** 数据源 */
 const { collaborators } = useCollaborators()
-/** 节点记录 */
-const masonryWrapNodeRef = ref<HTMLDivElement>(document.createElement('div'))
+/** 列表容器 */
+const masonryContainerNodeRef = ref<HTMLDivElement>(document.createElement('div'))
+/** 列表节点 */
+const masonryListNodeRef = ref<HTMLDivElement>(document.createElement('div'))
 /** masonry实例 */
-const masonryInstance = ref<masonry>(new masonry(masonryWrapNodeRef.value))
+const masonryInstance = ref<masonry>(new masonry(masonryListNodeRef.value))
 
 /** 是否是宽屏，设置图片为对应大小央视 */
 const isWide = useMedia(ref('(min-width: 1080px)'))
@@ -32,22 +34,30 @@ const relayoutHandle = debounce(
   { trailing: true }
 )
 
+const resizeObserver = new ResizeObserver(relayoutHandle)
+
 onMounted(() => {
-  masonryInstance.value = new masonry(masonryWrapNodeRef.value, {
+  masonryInstance.value = new masonry(masonryListNodeRef.value, {
     itemSelector: '.grid-item',
     gutter: 15,
     fitWidth: true,
     // 这里我也忘了原来什么情况下有nano，可能原本有后来又没了（需求变更），这里先写下
     columnWidth: size.value === CardSize.nano ? 100 : 240
   })
+
+  /** 监听布局变化（侧边菜单展开等） */
+  resizeObserver.observe(masonryContainerNodeRef.value)
 })
 onBeforeUnmount(() => {
   masonryInstance.value.destory()
+  resizeObserver.disconnect()
 })
 </script>
 <template>
-  <div class="com_waterfall_container">
-    <div class="com_waterfall_list" ref="masonryWrapNodeRef">
+  <!-- com_waterfall_container宽随父容器 -->
+  <div class="com_waterfall_container" ref="masonryContainerNodeRef">
+    <!-- com_waterfall_list宽随子元素 -->
+    <div class="com_waterfall_list" ref="masonryListNodeRef">
       <!-- grid-item 这个class是给masonry定位子元素用的 -->
       <card-item
         v-for="item in collaborators"
@@ -62,11 +72,10 @@ onBeforeUnmount(() => {
 </template>
 <style lang="scss">
 .com_waterfall_container {
-  background-color: #eee;
   color: rgba(#000, 0.87);
   letter-spacing: 0.01071em;
+  max-width: 100%;
   font-weight: 400;
-  width: 75vw;
   margin-left: auto;
   margin-right: auto;
   &,
--- src/views/Collaborator/store/data.ts
+++ src/views/Collaborator/store/data.ts
@@ -3,7 +3,10 @@ import { nanoid } from 'nanoid'
 
 const imgs: string[] = [
   'https://img.acgdmzy.com:45112/images/2021/08/22/08579907e581.webp',
-  'https://i2.hdslb.com/bfs/archive/307afe2558b4bb3a4a655d284a47459b9c6cd3fa.jpg'
+  'https://i0.hdslb.com/bfs/archive/307afe2558b4bb3a4a655d284a47459b9c6cd3fa.jpg',
+  'https://i0.hdslb.com/bfs/bangumi/image/8b1657cc9ded02796ce317ff7e1fd36f2dc9a0bb.jpg',
+  'https://i0.hdslb.com/bfs/manga-static/1cecbe6033d31cc9a49f4c1df88258a0abf72e07.jpg',
+  'https://i0.hdslb.com/bfs/archive/ab734a5ec06f568c27ea2212bbfb5e22d31284ae.jpg'
 ]
 
 const mock = (): Card[] => {
