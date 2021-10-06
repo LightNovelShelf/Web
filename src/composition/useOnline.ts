1ca7ca1289e93c6ed5e7858460f3364a5f7bbcb6
inori-lover <337938188@qq.com>
2021-10-06 23:08:54 +0800

    feat: 使用vue3的ref组件代替繁杂的SignalrEvtEmiter；添加重连逻辑

--- src/composition/useOnline.ts
+++ src/composition/useOnline.ts
@@ -1,26 +1,8 @@
-import { ref, Ref, onMounted, onBeforeUnmount } from 'vue'
+import { Ref } from 'vue'
 
-import { signalrEvtEmiter } from '@/services/internal/request'
-
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const NOOP = () => {}
+import { isConnected } from '@/services/internal/request'
 
 /** 检查是否online（包括是否链接上了ws） */
 export function useOnline(): Ref<boolean> {
-  const online = ref<boolean>(false)
-  /** 清除监听 */
-  const cleaner = ref<() => void>(NOOP)
-
-  onMounted(() => {
-    try {
-      cleaner.value = signalrEvtEmiter.onStatusChange((connected) => (online.value = connected))
-      online.value = signalrEvtEmiter.lastConnectedStatus()
-    } catch (e) {
-      /**  */
-    }
-  })
-
-  onBeforeUnmount(() => cleaner.value())
-
-  return online
+  return isConnected
 }
