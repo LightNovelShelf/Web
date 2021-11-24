1abbc5b952e4e8a228ae5e3cd2721136a69f7aa7
wuyu8512 <wuyu8512@gmail.com>
2021-11-24 16:51:43 +0800

    add quasar

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import './registerServiceWorker'
 import router from './router'
 import { naive } from './plugins/naive-ui'
 import SvgIcon from '@/plugins/naive-ui/components/SvgIcon.vue'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
-const app = createApp(App)
+const app = createApp(App).use(Quasar, quasarUserOptions)
 app.component('svg-icon', SvgIcon)
 app.use(createPinia()).use(router).use(naive).mount('#app')
---
+++ src/quasar-user-options.js
@@ -0,0 +1,12 @@
+import './styles/quasar.scss'
+import iconSet from 'quasar/icon-set/mdi-v4.js'
+import lang from 'quasar/lang/zh-CN.js'
+import '@quasar/extras/mdi-v4/mdi-v4.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {},
+  lang: lang,
+  iconSet: iconSet
+}
