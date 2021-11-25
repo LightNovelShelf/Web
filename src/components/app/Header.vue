706f1da2e4f1ba5c03ebaba6f5954399cbbd4701
wuyu8512 <wuyu8512@gmail.com>
2021-11-25 17:26:23 +0800

    partially migrated to quasar

--- src/components/app/Header.vue
+++ src/components/app/Header.vue
@@ -1,27 +1,38 @@
 <template>
   <n-layout-header class="header" bordered>
     <n-space class="header-title">
-      <n-tooltip placement="bottom-start">
-        <template #trigger>
-          <svg-icon :size="24" :path="icon.mdiInformation" />
-        </template>
-        欢迎来稿一个新的网站图标
-      </n-tooltip>
+      <q-btn flat dense round aria-label="Menu" :icon="icon.mdiMenu" />
+
+      <div class="row flex-center">
+        <q-icon :name="icon.mdiInformation" />
+        <q-tooltip anchor="bottom right" self="top right"> 欢迎来稿一个新的网站图标 </q-tooltip>
+      </div>
+
       <div @click="changAppName">{{ appName }}</div>
     </n-space>
 
-    <n-input class="search" autosize v-model:value="search" type="text" placeholder="搜索" />
+    <q-input dense outlined square v-model="search" placeholder="搜索" />
+
     <div style="flex: 1"></div>
     <n-space class="action" align="center" justify="center">
-      <n-badge dot>
-        <n-text>消息</n-text>
-      </n-badge>
-      <n-badge dot>
-        <n-text>动态</n-text>
-      </n-badge>
-      <n-badge dot>
-        <n-text>历史记录</n-text>
-      </n-badge>
+      <q-btn round dense flat>
+        <q-badge color="red" text-color="white" floating> 222 </q-badge>
+        <q-tooltip>通知</q-tooltip>
+        <svg-icon :path="icon.mdiBell"></svg-icon>
+      </q-btn>
+
+      <q-btn round dense flat label="动态">
+        <q-badge color="red" text-color="white" rounded floating></q-badge>
+      </q-btn>
+
+      <q-btn round dense flat>
+        <q-badge color="red" text-color="white" floating> 99+ </q-badge>
+        <q-tooltip>通知</q-tooltip>
+        <svg-icon :path="icon.mdiMessageText"></svg-icon>
+      </q-btn>
+
+      <div />
+
       <n-popover :show-arrow="false" placement="bottom-end" trigger="click">
         <template #trigger>
           <n-avatar round size="medium" src="https://q.qlogo.cn/headimg_dl?spec=100&dst_uin=1789263779" />
@@ -39,9 +50,11 @@
 import { computed, defineComponent, ref } from 'vue'
 import { icon } from '@/plugins/naive-ui'
 import { useAppStore } from '@/store'
+import SvgIcon from '@/plugins/naive-ui/components/SvgIcon.vue'
 
 export default defineComponent({
   name: 'Header',
+  components: { SvgIcon },
   setup() {
     const appStore = useAppStore()
 
@@ -66,6 +79,7 @@ export default defineComponent({
   padding: 0 var(--side-padding);
 
   .header-title {
+    align-items: center;
     font-size: 24px;
     width: calc(var(--slider-width) - var(--side-padding) + var(--content-padding-w));
   }
