e3e9351c4b3f2d95bb543b161729ec04ddfb40d3
noreply <noreply@github.com>
2021-11-25 09:45:19 +0800

    use vue-cli@5

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import router from './router'
 import { naive } from './plugins/naive-ui'
 import SvgIcon from '@/plugins/naive-ui/components/SvgIcon.vue'
 import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
+import quasarUserOptions from '@/plugins/quasar/options'
 
-const app = createApp(App).use(Quasar, quasarUserOptions)
+const app = createApp(App)
 app.component('svg-icon', SvgIcon)
-app.use(createPinia()).use(router).use(naive).mount('#app')
+app.use(createPinia()).use(router).use(naive).use(Quasar, quasarUserOptions).mount('#app')
