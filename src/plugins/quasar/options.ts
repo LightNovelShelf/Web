835d69b1a8980a35eb619c905b345aef48eee81a
wuyu8512 <wuyu8512@gmail.com>
2021-12-03 10:58:09 +0800

    up

--- src/plugins/quasar/options.ts
+++ src/plugins/quasar/options.ts
@@ -1,13 +1,17 @@
 import '@/styles/quasar.scss'
 import lang from 'quasar/lang/zh-CN.js'
+import { Notify } from 'quasar'
 
 export default {
   config: {
     dark: 'auto',
     screen: {
       bodyClasses: true
-    }
+    },
+    notify: {}
   },
-  plugins: {},
+  plugins: {
+    Notify
+  },
   lang: lang
 }
