7c9ea22f2e546cc468ca725be2317c7019e208e6
inori-lover <337938188@qq.com>
2021-12-04 23:51:48 +0800

    fix: 修正vue文件类型报错

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,35 +1,5 @@
-/* eslint-disable */
-import { DefineComponent } from 'vue'
-
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent
   export default component
-}
-declare module 'masonry-layout' {
-  class masonry {
-    constructor(
-      node: string | HTMLElement,
-      opt?: {
-        /** 选择器 */
-        itemSelector?: string
-        /** 间隔 */
-        gutter?: number
-        /** 容器符合内容宽度方便居中 */
-        fitWidth?: boolean
-        /** 设置fitWidth后 columnWidth 需要设置为 数字宽度 */
-        columnWidth?: number
-      }
-    )
-    layout(): void
-    destroy(): void
-  }
-
-  export default masonry
-}
-
-declare global {
-  interface Window {
-    Sanitizer: any
-  }
 }
