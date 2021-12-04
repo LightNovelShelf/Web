0da9aaa2560f8581d391821017e3d99d10f7ffa0
inori-lover <337938188@qq.com>
2021-12-04 23:51:48 +0800

    fix: 修正vue文件类型报错

---
+++ src/shims-global.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  interface Window {
+    Sanitizer: any
+  }
+}
+
+// extend global 需要 .d.ts 是一个module
+// @url https://stackoverflow.com/questions/47736473/how-to-define-global-function-in-typescript/47736563#47736563
+export {}
