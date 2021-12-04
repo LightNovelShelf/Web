7c47c706ef60a9f5e9de63f79481228084df2a5a
noreply <noreply@github.com>
2021-12-05 02:12:16 +0800

    add example

--- src/composition/useTimeout.ts
+++ src/composition/useTimeout.ts
@@ -2,7 +2,7 @@ import { AnyVoidFunc } from '@/types/utils'
 import { getCurrentInstance, onActivated, onDeactivated, onUnmounted, ref } from 'vue'
 
 /**
- * 延时执行，当组件被 deactive 或者 unmount 的时候就不执行相关cb
+ * 延时执行，当组件被 deactivate 或者 unmount 的时候就不执行相关cb
  *
  * @description
  * 只能在 setup 或者 生命周期内同步执行
@@ -11,20 +11,38 @@ export function useTimeout(cb: AnyVoidFunc, delay?: number) {
   /** 获取当前实例 */
   const instance = getCurrentInstance()
   /** 计算当前组件时候是激活状态 */
-  const currentActived = !!(instance?.isMounted && !instance?.isDeactivated)
+  const currentActivated = !!(instance?.isMounted && !instance?.isDeactivated)
 
   /** 组件是否可见 */
-  const actived = ref(currentActived)
+  const activated = ref(currentActivated)
   const _cb = cb.bind(this)
 
   setTimeout(() => {
-    if (!actived.value) {
+    if (!activated.value) {
       return
     }
     _cb()
   }, delay)
 
-  onDeactivated(() => (actived.value = false))
-  onActivated(() => (actived.value = true))
-  onUnmounted(() => (actived.value = false))
+  onDeactivated(() => (activated.value = false))
+  onActivated(() => (activated.value = true))
+  onUnmounted(() => (activated.value = false))
+}
+
+/**
+ * 延时执行，当返回值被置为false就不执行
+ */
+export function useTimeoutOutVue(cb: AnyVoidFunc, delay?: number) {
+  /** 组件是否可见 */
+  const activated = ref(true)
+  const _cb = cb.bind(this)
+
+  setTimeout(() => {
+    if (!activated.value) {
+      return
+    }
+    _cb()
+  }, delay)
+
+  return activated
 }
