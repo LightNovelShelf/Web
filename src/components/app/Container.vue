4291add2ff02b1e5059f01f5b07d530fc188890f
Azuki-Azusa <cai980129@vip.qq.com>
2021-12-07 14:01:13 +0900

    setting page with darkmode & fontsize

--- src/components/app/Container.vue
+++ src/components/app/Container.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page-container>
+  <q-page-container class="body">
     <div :style="{ padding: '12px' }">
       <router-view v-slot="{ Component }">
         <!-- TODO 想加个q-transition--fade，但测试下来有点问题 -->
