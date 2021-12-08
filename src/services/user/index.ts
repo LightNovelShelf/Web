0468dd06afbb4f2dc1bece1c00e498e9bef627e7
inori_lover <337938188@qq.com>
2021-12-08 15:16:21 +0800

    feat: 建立路由表概念；建立type格式

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -1,20 +1,17 @@
 import { requestWithFetch } from '@/services/internal/request'
-import { LoginRes } from '@/services/user/type'
+import { PATH } from '@/services/path'
 
-const HOST = `${process.env.VUE_APP_API_SERVER}/api/user/`
+import * as Types from './type'
+export { Types as UserServicesTypes }
 
 export function login(email: string, password: string, token: string) {
-  const data = { email, password, token }
-  return requestWithFetch(HOST + 'login', {
-    method: 'POST',
-    data
+  return requestWithFetch<Types.Login.Res, Types.Login.Param>(PATH.USER_LOGIN, {
+    payload: { email, password, token }
   })
 }
 
 export function refreshToken(token: string) {
-  const data = { token }
-  return requestWithFetch(HOST + 'refresh_token', {
-    method: 'POST',
-    data
+  return requestWithFetch<Types.RefreshToken.Res, Types.RefreshToken.Param>(PATH.USER_REFRESH_TOKEN, {
+    payload: { token }
   })
 }
