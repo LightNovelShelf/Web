e9e6fd97cd0daa5af604c9842f135d11056a99c8
inori_lover <337938188@qq.com>
2021-12-09 10:12:21 +0800

    feat: 丢掉复杂概念，简化写法；不再依赖第一次连接

--- src/services/utils/useServerNotify.ts
+++ src/services/utils/useServerNotify.ts
@@ -1,41 +1,13 @@
-import { NOOP } from '@/utils/const'
-import { onUnmounted, ref, watch } from 'vue'
-import { isConnected } from '.'
+import { onUnmounted } from 'vue'
 import { subscribeWithSignalr } from '../internal/request/signalr'
 
 /** 订阅某个接口返回 */
 export async function useServerNotify<Res = unknown>(methodName: string, cb: (res: Res) => void) {
-  const unsubscribor = ref(NOOP)
-
-  /** 取消订阅 */
-  function unsubscribe() {
-    try {
-      unsubscribor.value()
-    } catch (e) {
-      // ignore
-    }
-  }
-
-  if (isConnected.value) {
-    unsubscribor.value = await subscribeWithSignalr(methodName, cb)
-  } else {
-    const stopWatch = watch(isConnected, async (connected) => {
-      if (connected) {
-        try {
-          unsubscribor.value = await subscribeWithSignalr(methodName, cb)
-          // 监听一次就够了, signalr断开了也会记住相关订阅回调
-          // 不停止的话会监听多次, 更麻烦
-          stopWatch()
-        } catch (e) {
-          //
-        }
-      }
-    })
-  }
+  const unsubscribor = subscribeWithSignalr(methodName, cb)
 
   // 卸载时自动取消订阅
   // 暂不考虑 unactivate 情况, 考虑了之后就代表还得考虑 activate 重连
-  onUnmounted(unsubscribe)
+  onUnmounted(unsubscribor)
 
-  return unsubscribe
+  return unsubscribor
 }
