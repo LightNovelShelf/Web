157422377251048822c64af7502a292c7c180537
wuyu8512 <wuyu8512@gmail.com>
2021-12-09 17:38:52 +0800

    新增未登录提示

--- src/components/app/Container/AuthenticationGuard.vue
+++ src/components/app/Container/AuthenticationGuard.vue
@@ -4,17 +4,25 @@
 import { defineComponent } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { unAuthenticationNotify } from '@/utils/biz/unAuthenticationNotify'
+import { useQuasar } from 'quasar'
 
 /** 监听鉴权失败消息 */
 export default defineComponent(() => {
   const route = useRoute()
   const router = useRouter()
+  const $q = useQuasar()
 
   unAuthenticationNotify.useSubscribe(() => {
     // 必须要到这里才解构取值，不然解构后就不reactive了
     const { name, fullPath } = route
 
     if (name !== 'Login') {
+      $q.notify({
+        type: 'negative',
+        timeout: 1500,
+        position: 'bottom',
+        message: '此操作必须登录，正在前往登录页面'
+      })
       router.replace({ name: 'Login', query: { from: encodeURIComponent(fullPath) } })
     }
   })
--- src/services/utils/useServerNotify.ts
+++ src/services/utils/useServerNotify.ts
@@ -3,11 +3,11 @@ import { subscribeWithSignalr } from '../internal/request/signalr'
 
 /** 订阅某个接口返回 */
 export async function useServerNotify<Res = unknown>(methodName: string, cb: (res: Res) => void) {
-  const unsubscribor = subscribeWithSignalr(methodName, cb)
+  const unSubscriber = subscribeWithSignalr(methodName, cb)
 
   // 卸载时自动取消订阅
-  // 暂不考虑 unactivate 情况, 考虑了之后就代表还得考虑 activate 重连
-  onUnmounted(unsubscribor)
+  // 暂不考虑 onDeactivated 情况, 考虑了之后就代表还得考虑 activate 重连
+  onUnmounted(unSubscriber)
 
-  return unsubscribor
+  return unSubscriber
 }
