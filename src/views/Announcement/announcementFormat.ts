ba22e6c9ffa8bdf0ef91136ea178e2c2db5ea242
noreply <noreply@github.com>
2021-12-15 10:47:22 +0800

    使用dom api获取html文本

--- src/views/Announcement/announcementFormat.ts
+++ src/views/Announcement/announcementFormat.ts
@@ -1,32 +1,34 @@
 import { useToNow } from '@/composition/useToNow'
 import { computed, Ref } from 'vue'
+import sanitizerHtml from '@/utils/sanitizeHtml'
 
 export interface Announcement {
   Id: number
   Title: string
-  CreateTime: DateConstructor
   Create: string
   Before: Ref<string>
   Content: string
   PreviewContent: string
 }
 
-function matchReg(str: string): string {
-  const reg = /<\/?.+?\/?>/g
-  return str.replace(reg, '')
+function getPreview(html: string): string {
+  let div = document.createElement('div')
+  div.innerHTML = sanitizerHtml(html)
+  const text = div.textContent
+  div = null
+  return text.length > 50 ? text.substring(0, 50) + '...' : text
 }
 
 export function announcementFormat(element): Announcement {
-  const ele: any = {}
-  ele.Create =
-    element.CreateTime.getFullYear() + '.' + (element.CreateTime.getMonth() + 1) + '.' + element.CreateTime.getDate()
-  ele.Before = useToNow(computed(() => element.CreateTime))
-  ele.PreviewContent = matchReg(element.Content)
-  ele.Content = element.Content
-  ele.PreviewContent = ele.PreviewContent.length > 50 ? ele.PreviewContent.substring(0, 50) + '...' : ele.PreviewContent
-  ele.Title = element.Title
-  ele.Id = element.Id
-  return ele
+  return {
+    Id: element.Id,
+    Create:
+      element.CreateTime.getFullYear() + '.' + (element.CreateTime.getMonth() + 1) + '.' + element.CreateTime.getDate(),
+    Before: useToNow(computed(() => element.CreateTime)),
+    PreviewContent: getPreview(element.Content),
+    Content: element.Content,
+    Title: element.Title
+  }
 }
 
 export function announcementListFormat(announcementList): Announcement[] {
