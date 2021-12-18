375c7047d4f94bf7f5c7d63fc05deaf599ad714c
inori-lover <337938188@qq.com>
2021-12-18 14:01:23 +0800

    feat: DB抽象添加约束写入值的能力；添加自动toRaw操作降低使用难度

--- src/utils/storage/db/index.ts
+++ src/utils/storage/db/index.ts
@@ -1,7 +1,9 @@
 import { DB } from './base'
 export { DB }
 
-export const shelfDB = new DB('USER_SHELF', '用于储存用户书架数据')
+import type { SheldItem } from '@/types/shelf'
+
+export const shelfDB = new DB<SheldItem>('USER_SHELF', '用于储存用户书架数据')
 export const userSettingDB = new DB('SETTING', '设置缓存')
 export const signalrCacheDB = new DB('SIGNALR_CACHE', '请求缓存储存')
 export const userAuthenticationDB = new DB('USER_AUTHENTICATION', '用户授权信息')
