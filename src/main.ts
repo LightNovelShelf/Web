33fc1569c0d4313e1acef096bd8c6301b403daf0
wuyu8512 <wuyu8512@gmail.com>
2021-12-20 12:37:24 +0800

    引入图片预览组件

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,15 @@ import { QGridItem, QGrid } from '@/plugins/quasar/components'
 const app = createApp(App)
 app.component('q-grid', QGrid)
 app.component('q-grid-item', QGridItem)
+
+import Viewer from 'v-viewer'
+import 'viewerjs/dist/viewer.css'
+app.use(Viewer, {
+  defaultOptions: {
+    navbar: false
+  }
+})
+
 app.use(createPinia()).use(router).use(Quasar, quasarUserOptions).mount('#app')
 
 export default app
