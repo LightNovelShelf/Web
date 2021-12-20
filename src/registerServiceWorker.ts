fd2424de148827e687da68d13a4be79fc62da103
wuyu8512 <wuyu8512@gmail.com>
2021-12-20 17:32:57 +0800

    up

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,8 +1,5 @@
-/* eslint-disable */
-
-import { register, unregister } from 'register-service-worker'
-
-// unregister()
+import { Notify } from 'quasar'
+import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -22,6 +19,12 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated() {
       console.log('New content is available; please refresh.')
+      Notify.create({
+        message: '有新内容可用了，请刷新或重新打开网页',
+        position: 'bottom',
+        timeout: 1500,
+        type: 'positive'
+      })
     },
     offline() {
       console.log('No internet connection found. App is running in offline mode.')
