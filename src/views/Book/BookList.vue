c23b6925a93a55ac0ca1d48fb56efb833536017a
noreply <noreply@github.com>
2021-12-24 14:27:01 +0800

    支持搜索功能

--- src/views/Book/BookList.vue
+++ src/views/Book/BookList.vue
@@ -48,7 +48,7 @@ import { NOOP } from '@/const/empty'
 import { useInitRequest } from '@/composition/biz/useInitRequest'
 
 defineComponent({ QGrid, QGridItem })
-const props = defineProps<{ page: string; order: string }>()
+const props = defineProps<{ page: string; order: 'new' | 'view' | 'latest' }>()
 
 const options = [
   {
@@ -105,7 +105,7 @@ const order = computed({
   }
 })
 
-const request = useTimeoutFn(function (page: number = currentPage.value, order: string = props.order) {
+const request = useTimeoutFn(function (page = currentPage.value, order = props.order) {
   return getBookList({ Page: page, Order: order, Size: 24 }).then((serverData) => {
     bookData.value = serverData.Data
     pageData.value.totalPage = serverData.TotalPages
