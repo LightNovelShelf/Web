c9695eb7b38fdebba24eaaa1f9477c4907b649e7
wuyu8512 <wuyu8512@gmail.com>
2021-12-24 17:06:10 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
   </q-layout>
 </template>
 
-<script lang="tsx">
-import { defineComponent, watch } from 'vue'
+<script lang="tsx" setup>
+import { computed, defineComponent, watch, watchEffect } from 'vue'
 import { AppSider, AppHeader, AppContainer } from '@/components/app/index'
 import { useQuasar } from 'quasar'
 import { useServerNotify } from '@/services/utils/useServerNotify'
@@ -17,59 +17,57 @@ import { useAppStore } from '@/store'
 import { longTermToken } from '@/utils/session'
 import { getMyInfo } from '@/services/user'
 
-export default defineComponent({
-  components: {
-    AppSider,
-    AppHeader,
-    AppContainer
-  },
-  setup() {
-    const $q = useQuasar()
-    $q.loadingBar.setDefaults({
-      color: 'purple',
-      size: '2px',
-      position: 'top'
-    })
+defineComponent({ AppSider, AppHeader, AppContainer })
 
-    const appStore = useAppStore()
-    const settingStore = useSettingStore()
-    settingStore.init()
+const $q = useQuasar()
+$q.loadingBar.setDefaults({
+  color: 'purple',
+  size: '2px',
+  position: 'top'
+})
 
-    const getUser = async () => {
-      const token = await longTermToken.get()
-      if (token) {
-        appStore.user = await getMyInfo()
-      }
-    }
-    getUser()
+const appStore = useAppStore()
+const settingStore = useSettingStore()
+settingStore.init()
 
-    // 字体设置
-    const style = document.createElement('style')
-    style.type = 'text/css'
-    style.id = 'read_style'
-    document.head.append(style)
-    watch(
-      () => appStore.user?.Font,
-      () => {
-        let fontUrl = appStore.user?.Font
-        if (fontUrl) {
-          if (!fontUrl.startsWith('http')) fontUrl = VUE_APP_API_SERVER + fontUrl
-          style.innerHTML = `@font-face{font-family:read;src:url(${fontUrl});}`
-        }
-      }
-    )
-
-    useServerNotify('OnMessage', (message: string) => {
-      $q.notify({
-        position: 'top',
-        html: true,
-        message: sanitizerHtml(message),
-        timeout: 2500
-      })
-    })
-
-    return {}
+const getUser = async () => {
+  const token = await longTermToken.get()
+  if (token) {
+    appStore.user = await getMyInfo()
   }
+}
+getUser()
+
+// 字体设置
+const style = document.createElement('style')
+style.type = 'text/css'
+style.id = 'read_style'
+document.head.append(style)
+watch(
+  () => appStore.user?.Font,
+  () => {
+    let fontUrl = appStore.user?.Font
+    if (fontUrl) {
+      if (!fontUrl.startsWith('http')) fontUrl = VUE_APP_API_SERVER + fontUrl
+      style.innerHTML = `@font-face{font-family:read;src:url(${fontUrl});}`
+    }
+  }
+)
+
+useServerNotify('OnMessage', (message: string) => {
+  $q.notify({
+    position: 'top',
+    html: true,
+    message: sanitizerHtml(message),
+    timeout: 2500
+  })
+})
+
+let color = computed(() => ($q.dark.isActive ? '#263238' : '#1976D2'))
+
+watchEffect(() => {
+  let metaThemeColor = document.querySelector('meta[name=theme-color]')
+  metaThemeColor.setAttribute('content', color.value)
 })
 </script>
 
