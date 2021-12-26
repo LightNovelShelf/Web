b00e2974ad1f4fce873f7f416d625ed59c453311
inori-lover <337938188@qq.com>
2021-12-26 21:47:15 +0800

    feat: 推翻书籍和文件夹分开储存的做法，这样会在排序的时候让逻辑很难写（书籍的index和文件夹的index需要跨数据结构联动）

--- src/types/shelf.ts
+++ src/types/shelf.ts
@@ -2,15 +2,21 @@ import { BookServicesTypes } from '@/services/book'
 
 /** 书架书籍 */
 export interface ShelfBook extends BookServicesTypes.BookInList {}
+
+/** 书架文件夹子数据 */
+export interface ShelfFolderChild {
+  id: string
+  type: ShelfItemType
+}
 /** 书架文件夹 */
 export interface ShelfFolder {
   /** 文件夹Id，尽量跟Book同名方便模板书写 */
   Id: string
   /** 文件夹名称，尽量跟Book同名方便模板书写 */
   Title: string
-  /** 文件夹内书籍 */
-  children: ShelfBookItem[]
-  /** 更改时间，iso格式 */
+  /** 文件夹内子数据 */
+  children: ShelfFolderChild[]
+  /** 更改时间，iso格式字符串 */
   updateAt: string
 }
 
@@ -27,11 +33,11 @@ export interface ShelfCommonItem {
   /** 类型 */
   type: ShelfItemType
   /** Id */
-  Id: string
+  id: string
   /** 次序 */
   index: number
   /** 父级文件夹ID，不在文件夹的话就空数组 */
-  parent: string[]
+  parents: string[]
   /** 选中态 */
   selected?: boolean
 }
--- src/utils/storage/db/index.ts
+++ src/utils/storage/db/index.ts
@@ -1,11 +1,9 @@
 import { DB } from './base'
 export { DB }
 
-import type { ShelfBookItem, ShelfFolderItem, ShelfItem } from '@/types/shelf'
+import type { ShelfItem } from '@/types/shelf'
 
 export const shelfDB = new DB<ShelfItem>('USER_SHELF', '用于储存用户书架数据')
-export const shelfBookDB = new DB<ShelfBookItem>('USER_SHELF_BOOK', '用于储存书架中书籍类型的数据')
-export const shelfFolderDB = new DB<ShelfFolderItem>('USER_SHELF_FOLDER', '用于储存书架中文件夹类型的数据')
 export const userSettingDB = new DB('SETTING', '设置缓存')
 export const signalrCacheDB = new DB('SIGNALR_CACHE', '请求缓存储存')
 export const userAuthenticationDB = new DB('USER_AUTHENTICATION', '用户授权信息')
