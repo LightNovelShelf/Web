57c44c16ed190962e0392fcf1f19945cb69414d9
wuyu8512 <wuyu8512@gmail.com>
2022-01-07 10:57:11 +0800

    fix dark

--- src/plugins/quasar/dark.ts
+++ src/plugins/quasar/dark.ts
@@ -4,9 +4,10 @@ const Key: string = (VUE_APP_NAME || 'eBook_Shelf') + '_Dark'
 export const Dark = {
   get(): 'auto' | boolean {
     const result = localStorage.getItem(Key)
+    console.log(result)
     if (result) {
       if (result === 'true') return true
-      else if (result === 'false') return true
+      else if (result === 'false') return false
       else return 'auto'
     } else {
       return 'auto'
