3690d3ee61e1c860dc698b6ca04a66ff15dfd392
wuyu8512 <wuyu8512@gmail.com>
2022-01-28 14:42:49 +0800

    关闭书架入口

--- src/components/app/Sider.vue
+++ src/components/app/Sider.vue
@@ -90,6 +90,7 @@ const menuOptions: Array<Record<string, any>> = [
     label: '我的书架',
     key: 'MyShelf',
     route: 'MyShelf',
+    disabled: true,
     icon: icon.mdiFolderHeartOutline
   },
   {
