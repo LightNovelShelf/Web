447f8353bf112edde9cb81ca259dc9a16e7465b3
inori_lover <inori_lover@outlook.com>
2022-02-11 23:57:48 +0800

    feat: 适配新的数据结构下列表获取书籍的方式

--- src/views/MyShelf/components/ShelfBook.vue
+++ src/views/MyShelf/components/ShelfBook.vue
@@ -1,51 +1,22 @@
 <template>
-  <!-- q-intersection 功能上也是OK，但弄不明白用法，每次都进来时就触发 visibility -->
-  <!-- <q-intersection :threshold="0.01" :disable="disabeled" @visibility="queryItem"> -->
-  <div ref="wrapRef">
-    <book-card v-if="item.type === ShelfItemTypeEnum.BOOK" :book="book" />
-  </div>
-  <!-- </q-intersection> -->
+  <book-card v-intersection.once="queryItem" :book="book" />
 </template>
 
 <script lang="ts" setup>
 import BookCard from '@/components/BookCard.vue'
-import { usebookListStore as useBookListStore } from '@/store/bookListData'
-import { useShelfStore } from '@/store/shelf'
-import { ShelfBookItem, ShelfItemTypeEnum } from '@/types/shelf'
-import { ref, watch } from 'vue'
+import { useBookListStore } from '@/store/bookListData'
+import { ShelfBookItem } from '@/types/shelf'
+import { computed } from 'vue'
 
 const props = defineProps<{ item: ShelfBookItem }>()
-const wrapRef = ref<HTMLDivElement | null>(null)
 const listDataStore = useBookListStore()
-const shelfStore = useShelfStore()
-const book = listDataStore.getBook(props.item.id)
-const unWatch = watch(wrapRef, (ele, preEle, onClean) => {
-  if (ele) {
-    const ob = new IntersectionObserver(
-      ([item]) => {
-        if (item.intersectionRatio > 0) {
-          queryItem(true, () => {
-            unWatch()
-            ob.disconnect()
-          })
-        }
-      },
-      { threshold: 0, root: null, rootMargin: '10px' }
-    )
-
-    ob.observe(ele)
-
-    onClean(() => ob.disconnect())
-  }
-})
+const book = computed(() => listDataStore.getBook(props.item.id))
 
 /** 查询相关item */
-function queryItem(visible: boolean, clean?: () => void) {
+function queryItem(entry: IntersectionObserverEntry) {
   const { item } = props
-  if (visible && item) {
+  if (entry.isIntersecting && item) {
     listDataStore.queryBooks({ ids: [item.id] })
-
-    clean && clean()
   }
 }
 </script>
