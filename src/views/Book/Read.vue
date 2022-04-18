1d3fc8b8e1ae7c7e4ad50949855ca2b6be1169d5
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2022-04-18 22:00:02 +0800

    fix read page style

--- src/views/Book/Read.vue
+++ src/views/Book/Read.vue
@@ -26,7 +26,7 @@
         no-parent-event
         :max-width="$q.platform.is.mobile ? '90%' : '100%'"
       />
-      <div v-if="readSetting['showButton']" class="row justify-between q-gutter-md" style="margin-top: 24px">
+      <div v-if="readSetting['showButton']" class="row justify-between q-gutter-md" style="margin-top: 24px; clear: both;">
         <q-btn @click="prev" class="flex-space">上一章</q-btn>
         <q-btn @click="back" class="flex-space">目录</q-btn>
         <q-btn @click="next" class="flex-space">下一章</q-btn>
