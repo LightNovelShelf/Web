bd807182b999407eb4f67ec3dc3980023ebbdaed
baka-gourd <fanng233@gmail.com>
2022-06-30 17:15:37 +0800

    管理页面逻辑改进&新增设置 (#131)
    * 修复页面空缺与保存判断逻辑
    
    * 修复api
    
    * update api
    
    * 章节数现在可以更改了
    
    * 更新api&添加设置页
    
    * use watch
    
    * 书籍管理逻辑改进
    
    * 成功保存设置后提示
    
    Co-authored-by: wuyu8512 <wuyu8512@gmail.com>

--- src/services/chapter/types.ts
+++ src/services/chapter/types.ts
@@ -4,12 +4,28 @@ export interface GetChapterContentRequest {
   Convert?: 't2s' | 's2t'
 }
 
-export interface EditChapterContentRequest {
-  BookId: number
-  SortNum: number
+interface GetChapterEditInfoBySortNum {
+  BookId?: number
+  SortNum?: number
+}
+
+interface GetChapterEditInfoByCid {
+  Cid?: number
+}
+
+export type GetChapterEditInfo = GetChapterEditInfoBySortNum | GetChapterEditInfoByCid
+
+interface EditChapterContentRequestBySortNum extends GetChapterEditInfoBySortNum {
   Content?: string
   Title?: string
 }
+
+interface EditChapterContentRequestByCid extends GetChapterEditInfoByCid {
+  Content?: string
+  Title?: string
+}
+
+export type EditChapterContentRequest = EditChapterContentRequestBySortNum | EditChapterContentRequestByCid
 
 export interface DeleteChapterRequest {
   BookId: number
