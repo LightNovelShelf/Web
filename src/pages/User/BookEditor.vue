bfa49d8ff42fdbcf59075bcb36803611453ae738
noreply <noreply@github.com>
2026-08-15 12:55:22 +0800

    feat: 书籍编辑器保存按钮改为悬浮按钮组

--- src/pages/User/BookEditor.vue
+++ src/pages/User/BookEditor.vue
@@ -110,8 +110,14 @@
     <q-page-sticky position="top-right" :offset="[18, 18]" v-if="!siderShow">
       <q-btn fab icon="mdiArrowLeft" color="accent" @click="show = !show" />
     </q-page-sticky>
-    <drag-page-sticky>
-      <q-btn fab icon="mdiContentSave" color="secondary" :disable="getSaveState()" @click="save()"></q-btn>
+    <drag-page-sticky v-slot="{ isDragging }">
+      <q-fab icon="mdiPlus" direction="up" color="accent" :disable="isDragging">
+        <q-fab-action color="primary" @click="save" icon="mdiContentSave" :disable="isDragging || getSaveState()">
+          <q-tooltip transition-show="scale" transition-hide="scale" anchor="center left" self="center right">
+            保存
+          </q-tooltip>
+        </q-fab-action>
+      </q-fab>
     </drag-page-sticky>
   </q-page>
   <q-drawer
